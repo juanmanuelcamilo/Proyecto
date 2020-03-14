5cba573f9ad3446de6c95c9cf725e5a924744b8c
juanmanuelcamilo <juanmanuelcamilo@users.noreply.github.com>
2020-03-13 23:53:28 -0300

    Comentarios
    Comentamos algunas cosas que no vamos a realizar por ahora.

--- trabajo1/src/App.js
+++ trabajo1/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
-import Header from './Header';
-import Body from './Body';
-import Footer from './Footer';
+// import Header from './Header';
+import Body from './menu/Body';
+// import Footer from './Footer';
 
 class App extends Component{
   render() {
     return (
       <div>
-        <Header />
+        {/* <Header /> */}
         <Body />
-        <Footer />
+        {/* <Footer /> */}
       </div>
     )
   }
