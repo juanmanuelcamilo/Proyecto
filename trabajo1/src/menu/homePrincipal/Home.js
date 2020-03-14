06c93dca1bceb61638bc15ff51cf998ea67e7237
juanmanuelcamilo <juanmanuelcamilo@users.noreply.github.com>
2020-03-14 00:01:38 -0300

    Home
    Creamos el test del home de nuestra página que luego sera actualizado correctamente.

---
+++ trabajo1/src/menu/homePrincipal/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Esta va a ser el Home</h1>
+      </div>
+    )
+  }
+}
+
+export default Home
